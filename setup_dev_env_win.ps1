41e614347d4c60886ca4ceb67c7b59f87c74e64c
John Demme <john.demme@microsoft.com>
2020-01-30 15:25:31 -0800

    Should be ready for first GH workflow run

---
+++ setup_dev_env_win.ps1
@@ -0,0 +1,24 @@
+###
+###  Set up a build environment on Windows
+###
+
+# Change to "tools" dir
+$origDir = Get-Location
+mkdir "$PSScriptRoot/tools" tools -ErrorAction Ignore
+Set-Location "$PSScriptRoot/tools"
+
+# Install CapnProto
+$capnprotoVersion = "0.7.0"
+$capnprotoZip = "capnproto-c++-win32-$capnprotoVersion.zip"
+if (-not (Test-Path $capnprotoZip)) {
+    Invoke-WebRequest -Uri "https://capnproto.org/$capnprotoZip" -OutFile $capnprotoZip
+    Expand-Archive -Path $capnprotoZip -DestinationPath .
+    Move-Item "capnproto-tools-win32-$capnprotoVersion" capnproto-tools
+}
+$env:PATH="$env:PATH;$(Get-Location)/capnproto-tools"
+
+# Go back to whence we came
+Set-Location $origDir
+
+# Write string for GH build env var
+Write-Output "::set-env name=PATH::$env:PATH"
--- src/core/schema/EsiCoreAnnotations.capnp
+++ src/core/schema/EsiCoreAnnotations.capnp
@@ -3,6 +3,10 @@
 
 struct HWType {
     union {
-        bits @0 :Unit64; # Customize the number of bits used in hardware
+        bits @0 :UInt64; # Customize the number of bits used in hardware
+        fixed :group { # Signed fixed point type
+            whole @1 :UInt64; # Number of bits of whole
+            fraction @2 :UInt64; # Number of bits of fraction
+        }
     }
 }
