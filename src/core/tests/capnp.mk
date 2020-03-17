4db5914c9a996c7d9cd88ccc974eabb188ba81e7
John Demme <john.demme@microsoft.com>
2020-03-16 18:47:41 -0700

    Testing setup

--- src/core/tests/capnp.mk
+++ src/core/tests/capnp.mk
@@ -1,7 +1,11 @@
 .SUFFIXES: .capnp .capnp.txt .capnp.bin
 
 .capnp.bin.capnp.txt:
-	cat $*.capnp.bin | capnp decode /usr/include/capnp/schema.capnp CodeGeneratorRequest > $*.capnp.txt
+ifeq ($(OS), Windows_NT)
+	gci $*.capnp.bin | capnp decode ../../schema/CapnpSchema.capnp CodeGeneratorRequest > $*.capnp.txt
+else
+	$*.capnp.bin | capnp decode ../../schema/CapnpSchema.capnp CodeGeneratorRequest > $*.capnp.txt
+endif
 
 .capnp.capnp.bin:
 	capnp compile -I../../schema/ -o- $*.capnp > $*.capnp.bin
